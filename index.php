d11a5bc5471f39412ce10b3923f6087968766e80
Edigleysson Silva <edigleyssonsilva@gmail.com>
2019-04-14 16:48:54 -0300

    new release - data can be filtered by every column

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
     $ativos = [];
     $siglas = [];
     $header = [];
+    $filtros = [];
+    $response = [
+        'result' => []
+    ];
 
     $row = 1;
     while($data = fgetcsv($handle)){
@@ -27,72 +31,46 @@
         $row++;
     }
 
+    $filtros = array_intersect($header, array_keys($_GET));
 
-    $sigla = (isset($_GET['sigla']) && !empty($_GET['sigla'])) ? explode(',', $_GET['sigla']) : null;
+    // aplicando filtros nos ativos
+    foreach ($filtros as $filtro) {
+        $valor_filtro = (!empty($_GET[$filtro])) ? explode(',', $_GET[$filtro]) : null;
+
+        if(!is_null($valor_filtro)) {
+            $res = $ativos;
+            $filtrado = [];
+
+            foreach ($res as $_ativo) {
+                if(in_array($_ativo[$filtro], $valor_filtro)) {
+                    $filtrado[] = $_ativo;
+                }
+            }
+
+            $ativos = $filtrado;
+        }
+    }
+
+    // atributos
     $atributo = (isset($_GET['atributo']) && !empty($_GET['atributo'])) ? explode(',', $_GET['atributo']) : null;
 
-    // filtros
-    $classe_filtro = (isset($_GET['classe']) && !empty($_GET['classe'])) ? explode(',', $_GET['classe']) : null;
-    $setor_filtro = (isset($_GET['setor']) && !empty($_GET['setor'])) ? explode(',', $_GET['setor']) : null;
-
-    $filtered = [
-        'ativos' => []
-    ];
 
 
-    // verificando sigla
-    if(!is_null($sigla)) {
-        $indices = [];
-        foreach($sigla as $codigo_ativo){
-            if($index = array_search(strtoupper($codigo_ativo), $siglas)) $indices[] = $index;
-        }
-
-        foreach($indices as $indice){
-            $filtered['ativos'][] = $ativos[$indice];
-        }
-    }else {
-        $filtered['ativos'] = $ativos;
-    }
-
-
-    if(!is_null($classe_filtro)) {
-        $res = $filtered['ativos'];
-        $filtered['ativos'] = [];
-
-        foreach($res as $_ativo){
-            if(in_array($_ativo['classe'], $classe_filtro)) {
-                $filtered['ativos'][] = $_ativo;
-            }
-        }
-    }
-
-    if(!is_null($setor_filtro)) {
-        $res = $filtered['ativos'];
-        $filtered['ativos'] = [];
-
-        foreach($res as $_ativo){
-            if(in_array($_ativo['setor'], $setor_filtro)) {
-                $filtered['ativos'][] = $_ativo;
-            }
-        }
-    }
-
-
-    $final = ['result' => []];
-
-    // verificando atributo
     if(!is_null($atributo)) {
-        foreach($filtered['ativos'] as $ativo_filtrado) {
+        $filtrado = [];
+        foreach($ativos as $ativo_filtrado) {
             $ativo = [];
             foreach($atributo as $atr) {
                 $ativo[$atr] = $ativo_filtrado[$atr];
             }
-            $final['result'][] = $ativo;
+            $filtrado[] = $ativo;
         }
-    }else {
-        $final['result'] = $filtered['ativos'];
+        $ativos = $filtrado;
     }
 
-    echo json_encode($final);
-
+    echo json_encode(
+        [
+            'result' => $ativos
+        ]
+    );
 ?>
